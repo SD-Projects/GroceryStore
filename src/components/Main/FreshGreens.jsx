4257a1e5552b07fab730ce5c5f643d76614d2ba4
Steven <bxing.day@gmail.com>
2023-09-12 21:14:49 -0400

    working with tiles UI

--- src/components/Main/FreshGreens.jsx
+++ src/components/Main/FreshGreens.jsx
@@ -3,17 +3,17 @@ import sliderRight from "../../media/images/main/sliderRight.png"
 const FreshGreens = () => {
 
     return (
-        <div className="flex-initial p-2 max-sm:h-80 text-white">
-            <div className="flex flex-col absolute p-5">
-                <div className="flex flex-col gap-2 content-center">
+        <div className="p-2 max-sm:h-80 text-white">
+            <div className="flex flex-col absolute p-5 h-full justify-between">
+                <div>
                     <p className="text-xs">3 products</p>
                     <p className="font-medium">Fresh Greens</p>
-                    <div><button className="text-sm rounded py-2 px-5 bg-green-600">Check Products</button></div>
                 </div>
+                <div><button className="text-sm rounded py-2 px-5 bg-green-600">Check Products</button></div>
+
             </div>
             <img className="rounded-md w-full object-cover max-sm:h-full" src={sliderRight} />
         </div >
-
     )
 
 }
--- src/components/Main/SlidingTile.jsx
+++ src/components/Main/SlidingTile.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react"
 import tile1 from "../../media/images/main/slider1.png"
 import tile2 from "../../media/images/main/slider3.png"
 import tile3 from "../../media/images/main/slider4.png"
+import { BsArrowRight } from "react-icons/bs"
 
 const SlidingTile = () => {
 
     const [currSlide, setCurrSlide] = useState(0)
     const slideImages = [{
-        image: tile1, title: "Fresh Garden Products", price: "$24.25", buttonCaption: "Check Products", priceColor: "text-red-500",
+        image: tile1, title: "Fresh Garden Products", price: "$24.25", buttonCaption: "Check Products", priceColor: "text-red-500", buttonColor: "bg-green-500"
     },
     { image: tile2, title: "Fresh Vegetables", price: "$31.99", buttonCaption: "See Products", priceColor: "text-orange-400" },
     { image: tile3, title: "Fresh Fruits", price: "$19.55", buttonCaption: "Buy Products", priceColor: "text-green-700" }]
@@ -48,25 +49,27 @@ const SlidingTile = () => {
 
                 {slideImages.map((slide, index) => (
                     <>
-                        <div className={`${index === currSlide ? "text-white absolute " : "hidden"}`}>
-                            <div className="flex flex-col gap-2 content-center">
-                                <div className="text-4xl max-sm:text-2xl font-medium">{slide.title}</div>
-                                <div className={`${slide.priceColor} "text-4xl max-sm:text-2xl font-medium flex flex-row"`}>
-                                    <span className="text-xs font-normal self-center">FROM</span> {slide.price}
+                        <div className={`${index === currSlide ? "text-white absolute top-32 left-32 max-sm:top-40 max-sm:left-12" : "hidden"}`}>
+                            <div className="flex flex-col content-center">
+                                <div className="text-4xl max-sm:text-2xl font-medium w-72 h-24 max-sm:h-12">{slide.title}</div>
+                                <div className={`${slide.priceColor} " text-4xl max-sm:text-2xl font-medium flex flex-row "`}>
+                                    <span className="text-xs font-normal self-center ">FROM</span> {slide.price}
                                 </div>
-                                <div className="text-sm">
+                                <div className="text-sm h-24 font-extralight">
                                     <p>Freshly squeezed juices from our
                                         <br></br>
                                         organic fruits delivered to your door</p>
                                 </div>
-                                <div><button className="rounded py-2 px-7 bg-red-500">{slide.buttonCaption}</button></div>
+                                <div><button className={`${slide.buttonColor} " flex rounded py-2 px-7 items-center text-sm font-medium justify-between "`}>
+                                    {slide.buttonCaption}<BsArrowRight className="ml-3"/>
+                                </button></div>
                             </div>
                         </div>
                         <img
                             key={index}
                             src={slide.image}
                             alt={`Slide ${index}`}
-                            className={`rounded-md w-full object-cover ${index === currSlide ? '' : 'hidden'
+                            className={`rounded-md w-full object-cover max-sm:h-128 ${index === currSlide ? '' : 'hidden'
                                 }`}
                         />
                     </>
