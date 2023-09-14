040bbc18637b5f510cd23594ec79f89d9e067f6b
sdifeo <s.difeo2@gmail.com>
2023-09-13 23:26:44 -0400

    ui

--- src/components/Main/MainTiles.jsx
+++ src/components/Main/MainTiles.jsx
@@ -7,6 +7,19 @@ const MainTiles = () => {
     return (
         <div className="flex flex-row w-4/5 h-64 mx-auto max-sm:flex-col max-sm:w-full">
             <div className="flex-1 p-2">
+                <div className="relative">
+                    <div className="absolute">
+                        <p className="text-sm">4 Products</p>
+                        <p className="font-bold">Health Products</p>
+
+                        <button className="bg-green-500 py-2 px-2 text-sm text-white rounded">
+                            <div className="font-medium">
+                                Order Now!
+                            </div>
+                        </button>
+
+                    </div>
+                </div>
                 <img className="w-full h-full rounded-md object-cover max-sm:h-80" src={tile1} />
 
             </div>
